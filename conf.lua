cb6851923c0b1e982468230dfe3c53aa0dea00f8
Nanok Charles <konancharlesyao@gmail.com>
2021-03-03 13:15:09 +0000

    setup some project setting

--- conf.lua
+++ conf.lua
@@ -1,4 +1,9 @@
 
+-- Comment this condition if you use an IDE different from vscode
+if os.getenv("LOCAL_LUA_DEBUGGER_VSCODE") == "1" then
+    require("lldebugger").start()
+end
+  
 
 function love.conf(t)
 
